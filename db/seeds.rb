2176c456d4c909a1d2ea4b3d2fa22073c3fa4bba
Santiago Cardona <santiagocardo80@gmail.com>
2019-09-12 14:36:24 -0500

    Chatroom, login and users resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'santiagocardo', password: "123456")
+User.create(username: 'msalomedh', password: "123456")
+User.create(username: 'elbergon', password: "123456")
